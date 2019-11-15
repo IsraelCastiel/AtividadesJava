086ff9c60e9b321b4ffa81b1fe77b654336f4c89
DELL <DELL@ISRAEL-PC>
2019-11-15 11:58:44 -0400

    4)	Crie um programa que converta ano luz para Km, receba a distância da Terra e apresente em KM.

--- Exercicios/src/Exercicios/Programa4.java
+++ Exercicios/src/Exercicios/Programa4.java
@@ -9,11 +9,11 @@ public class Programa4 {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		double anoluz = 9460730777119.56;
+		final double ANOLUZ = 9460730777119.56;
 		double dist;
 		System.out.println("Qual a distancia em anos luz deseja calcular");
 		dist=teclado.nextDouble();
-		System.out.println("A distancia em anos luz é " + anoluz*dist+ " KM");
+		System.out.println("A distancia em anos luz é " + ANOLUZ*dist+ " KM");
 		
 	}
 
