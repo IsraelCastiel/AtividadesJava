658864c9a8ff1620c185cc52f014c1cefc38c09f
DELL <DELL@ISRAEL-PC>
2019-11-15 09:55:30 -0400

    1)	Dada o raio do círculo calcular sua área e o perímetro.

--- Exercicios/src/Exercicios/Programa1.java
+++ Exercicios/src/Exercicios/Programa1.java
@@ -1,10 +1,25 @@
+
+/*
+ *1)	Dada o raio do círculo calcular sua área e o perímetro. 
+ */
 package Exercicios;
+
+import java.util.Scanner;
 
 public class Programa1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner teclado = new Scanner(System.in);
+	float pi = 3.14f;
+	float area;
+	float raio;
+	float perimetro;
+	System.out.println("Informe o raio do circulo");
+	raio=teclado.nextFloat();
+	raio=(float) Math.pow(raio,2);
+	perimetro = 2*pi*raio;
+	System.out.println("A area do circulo é " + raio*pi + " e seu perimetro é " + perimetro);
+		
 	}
 
 }
