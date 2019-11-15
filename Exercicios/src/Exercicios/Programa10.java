dbc76a86ec9561decc513dfa12f84d0c90bcf3fe
DELL <DELL@ISRAEL-PC>
2019-11-15 15:00:41 -0400

    10)	As temperaturas da semana foram: Temperatura	Umidade 39ºC	     10% 45ºC	     14% 41ºC	     12% 42ºC	      9% 38ºC	      7% Apresente a média das temperaturas e umidade nesta semana

--- Exercicios/src/Exercicios/Programa10.java
+++ Exercicios/src/Exercicios/Programa10.java
@@ -1,10 +1,16 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class Programa10 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		float n1,n2,n3,n4,n5,n6,media;
+		System.out.println("Informe as temperaturas da semana");
+		n1=teclado.nextFloat();
+		n2=teclado.nextFloat();
+		n3=teclado.nextFloat();
+		n4=teclado.nextFloat();
+		n5=teclado.nextFloat();
+		media = (n1+n2+n3+n4+n5)/5;
+		System.out.println("A media da temperatura da semana foi: "+media+" Graus");	
 	}
-
 }
