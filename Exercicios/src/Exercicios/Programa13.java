367864a6d16ab6547a3c215764ab5ffffd0dfaa2
DELL <DELL@ISRAEL-PC>
2019-11-18 11:28:12 -0400

    13)	Uma empresa deseja contar quanto veículo são de determinada marca, receba a marca e o nome do veículo e apresente ao final do programa quantos são da marca FIAT

---
+++ Exercicios/src/Exercicios/Programa13.java
@@ -0,0 +1,31 @@
+/*13)	Uma empresa deseja contar quantos veículo são de determinada marca, 
+ * receba a marca e o nome do veículo e apresente ao final do programa quantos
+ *  são da marca FIAT. 
+ */
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Programa13 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int c = 0;
+		
+		String Marca[] = {"FIAT","FIAT","RENAULT","FIAT","LOGAN"};		
+		
+		for (Marca[c].compareTo("FIAT");c<5;c++) {
+		
+		     
+		if(Marca[c]=="FIAT"){
+		
+			
+			System.out.println(Marca[c]+" na posicao " + c);
+			
+		}
+			
+		}
+}
+}
+
+	
