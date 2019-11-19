beee0ea481272fdf1279560328f6b5a7402018da
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-11-19 16:53:58 -0400

    16)	Imprima os números primos até 200.

---
+++ Exercicios/src/Exercicios/Programa16.java
@@ -0,0 +1,43 @@
+package Exercicios;
+
+public class Programa16 {
+
+	static int total = 200;
+	public static void main(String[] args) {
+		
+	int v[] = new int [total];
+	for (int i = 2; i< total;i++) {
+		
+		v[i] = i;
+		
+	}
+	
+	for(int i = 2;i < total; i++) {
+		
+		if(v[i] !=0){
+			
+			for(int j = 2*i; j < total;j+=i) {
+				
+				v[j] = 0;
+				
+			}
+			
+		}
+		
+	}
+	
+	for(int i = 2;i < total;i++) {
+		
+		if (v[i] !=0){
+			
+			System.out.print(v[i] + " - " + "\n");
+			
+		}
+
+
+		
+	}
+
+  }
+
+}
