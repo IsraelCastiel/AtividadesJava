531da4f78a59c6a39753ea8492ec822b3615aa48
DELL <DELL@ISRAEL-PC>
2019-11-19 07:52:21 -0400

    15)	Imprima os números pares até 100.

---
+++ Exercicios/src/Exercicios/Programa15.java
@@ -0,0 +1,20 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Programa15 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int n1=1;n1<=100;n1++) {
+			
+			System.out.print(n1);
+						
+			
+		}
+	
+
+	}
+
+}
