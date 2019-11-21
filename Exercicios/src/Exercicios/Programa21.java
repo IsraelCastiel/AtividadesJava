bc38a7de880cd89d7f6aa38b152c993be9a4dc01
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-11-21 14:56:33 -0400

    Receba a palavra que o usuário digitar e atribua os seguintes valores 5 para vogais e 10 para consoantes, apresente o resultado do valor da palavra digitada.

---
+++ Exercicios/src/Exercicios/Programa21.java
@@ -0,0 +1,37 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Programa21 {
+
+	public static void main(String[] args) {
+		
+		Scanner tec = new Scanner(System.in);
+		int i;
+		String palavra;
+		
+		
+		System.out.println("informe uma palavra:");
+		palavra=tec.next();
+		
+		int qtd = palavra.length();
+		
+		for(i=0;i<qtd;i++) 
+			
+			System.out.print(palavra.charAt(i) + " ");
+			 System.out.println("\nPossue " + qtd + " letras.");
+		
+		
+		
+		
+		
+   
+		
+		
+		
+		
+		
+
+	}
+
+}
