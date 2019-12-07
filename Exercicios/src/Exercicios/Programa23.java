7b20fe43aa7e54efc172b584ecf21834d52acd46
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-12-07 19:21:09 -0400

    23)	Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo com o cargo, conforme a tabela abaixo. Faça um programa que leia o salário e o código do cargo de um funcionário e calcule o seu novo salário. Se o cargo do funcionário não estiver na tabela, ele deverá, então, receber 15% de aumento. Mostre o salário antigo, o novo salário e a diferença entre ambos.

--- Exercicios/src/Exercicios/Programa23.java
+++ Exercicios/src/Exercicios/Programa23.java
@@ -1,7 +1,8 @@
+/*23)	Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo com o cargo, conforme a tabela abaixo. Faça um programa que leia o salário e o código do cargo de um funcionário e calcule o seu novo salário. Se o cargo do funcionário não estiver na tabela, ele deverá, então, receber 15% de aumento. 
+ * Mostre o salário antigo, o novo salário e a diferença entre ambos. 
+ */
 package Exercicios;
-
 import java.util.Scanner;
-
 public class Programa23 {
 	public static void main(String[] args) {
 		Scanner tec =new Scanner(System.in);
