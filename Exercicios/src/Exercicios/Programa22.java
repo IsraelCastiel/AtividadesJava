28d8a6245df44c7d2e1af4b552f2108a3bdf4a22
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-12-07 18:27:02 -0400

    22)	Crie um programa que receba o valor da cotação em Dólar e Euro, também valor em reais a ser convertido, apresente os valores da conversão nas respectivas moedas.

---
+++ Exercicios/src/Exercicios/Programa22.java
@@ -0,0 +1,30 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Programa22 {
+	public static void main(String[] args) {
+	float dolar = (float) 4.13;
+	float euro = (float) 4.57;
+	float valor,resultadodolar,resultadoeuro,reais;	
+	Scanner tec=new Scanner(System.in);
+	System.out.println("Informe o valor em reais que sera convertido");
+	valor= tec.nextFloat();
+	resultadodolar = valor / dolar;
+	resultadoeuro = valor / euro;
+
+	System.out.println(" o valor em dolar é:" + resultadodolar + "e o resultado em euro e:" + resultadoeuro   );
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+
+	}
+
+}
