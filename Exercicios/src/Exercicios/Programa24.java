c9c6307d60551ce688d0f2befcc41937e5c1f546
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-12-07 20:52:21 -0400

    24)	Os jogadores fazem cadastro no time 1 ou time 2, quando terminar o cadastro apresentar os nomes dos jogadores de cada time, caso um dos times tenha menos jogadores que outros, solicitar o cadastro e exibir os jogadores cadastrados.

---
+++ Exercicios/src/Exercicios/Programa24.java
@@ -0,0 +1,32 @@
+/*24)	Os jogadores fazem cadastro no time 1 ou time 2,
+ quando terminar o cadastro apresentar os nomes dos jogadores de cada time, 
+caso um dos times tenha menos jogadores que outros, 
+solicitar o cadastro e exibir os jogadores cadastrados.
+*/
+
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Programa24 {
+	public static void main(String[] args) {
+		int i;
+		Scanner tec = new Scanner(System.in);
+		String time1[] = new String[7];
+		String time2[] = new String[7];			
+		System.out.println("Informe para o time 1 os nomes dos jogadores");
+		
+		for (i = 0; i < 7;i++) {
+		time1[i]=tec.next();
+		}
+		
+		System.out.println("Informe para o time 2 os nomes dos jogadores");
+
+		for (i = 0; i < 7;i++) {
+		time2[i]=tec.next();
+		}
+		
+		
+		
+	}
+}
