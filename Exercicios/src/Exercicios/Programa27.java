535f1c68cfb35121f6c5fae73e50e5e2d7ebfebb
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-12-09 09:31:36 -0400

    27)	Crie um algoritmo que tenha a seguinte saída, com o menor número de laços possíveis.
    1	3	5	7	9 	11
    2	4	6	8	10	12
     2	12	30	56	90	121

---
+++ Exercicios/src/Exercicios/Programa27.java
@@ -0,0 +1,19 @@
+/*27)	Crie um algoritmo que tenha a seguinte saída, com o menor número de 
+ * laços possíveis.
+ *  1	3	5	7	9 	11
+ 	2	4	6	8	10	12
+ 	2	12	30	56	90	121
+ * 
+ * 
+ * 
+ */
+package Exercicios;
+
+public class Programa27 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
