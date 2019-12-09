fc706c0f4b496899f419880c2d41ba5a63cb6be8
Israel C.Castiel <54750514+IsraelCastiel@users.noreply.github.com>
2019-12-09 09:26:20 -0400

    /*25)	As cartas do baralho possuem valores numéricos, de A = 1 até K = 13,  * leia a mão de um jogador com 5 cartas e apresente a maior e a  * menor carta, também apresente parabéns caso ele possua cartas iguais.  */

--- Exercicios/src/Exercicios/Programa25.java
+++ Exercicios/src/Exercicios/Programa25.java
@@ -1,3 +1,9 @@
+/*25)	As cartas do baralho possuem valores numéricos, de A = 1 até K = 13, 
+ * leia a mão de um jogador com 5 cartas e apresente a maior e a
+ * menor carta, também apresente parabéns caso ele possua cartas iguais. 
+ */
+
+
 package Exercicios;
 
 public class Programa25 {
